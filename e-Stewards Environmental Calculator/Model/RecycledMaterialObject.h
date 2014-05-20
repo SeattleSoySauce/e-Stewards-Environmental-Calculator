f939026c8d7169944f43a6224e59e44a5b5c0951
Angelo Godbey <angelo@godbeystudios.com>
2014-05-19 17:40:31 -0700

    Defined Recycled Material Object

--- e-Stewards Environmental Calculator/Model/RecycledMaterialObject.h
+++ e-Stewards Environmental Calculator/Model/RecycledMaterialObject.h
@@ -10,4 +10,32 @@
 
 @interface RecycledMaterialObject : NSObject
 
+@property (nonatomic, retain) NSString *material;
+
+@property double totalWeight;
+
+@property double carbonDioxideConstant;
+@property double leadConstant;
+@property double mercuryConstant;
+@property double cadmiumConstant;
+@property double arsenicConstant;
+@property double copperConstant;
+@property double goldConstant;
+@property double platinumConstant;
+@property double alluminumConstant;
+@property double steelConstant;
+
+@property double carbonDioxideTotalWeight;
+@property double leadTotalWeight;
+@property double mercuryTotalWeight;
+@property double cadmiumTotalWeight;
+@property double arsenicTotalWeight;
+@property double copperTotalWeight;
+@property double goldTotalWeight;
+@property double platinumTotalWeight;
+@property double alluminumTotalWeight;
+@property double steelTotalWeight;
+
+@property double greenHouseGasEmmisionsToal;
+
 @end
