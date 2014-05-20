ee2f8355639c0de5168f87e90e2202f385eeee3a
Angelo Godbey <angelo@godbeystudios.com>
2014-05-19 17:15:49 -0700

    Added Recycled Material Object

---
+++ e-Stewards Environmental Calculator/Model/RecycledMaterialObject.h
@@ -0,0 +1,13 @@
+//
+//  RecycledMaterialObject.h
+//  e-Stewards Environmental Calculator
+//
+//  Created by Angelo Godbey on 5/19/14.
+//  Copyright (c) 2014 Basel Action Network. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RecycledMaterialObject : NSObject
+
+@end
