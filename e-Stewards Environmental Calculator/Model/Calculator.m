a8e310646e0bd1b497f5b1e503b0f2cc94a02d5c
Angelo Godbey <angelo@godbeystudios.com>
2014-05-19 17:40:56 -0700

    Created Calculator Model Object

---
+++ e-Stewards Environmental Calculator/Model/Calculator.m
@@ -0,0 +1,14 @@
+//
+//  Calculator.m
+//  e-Stewards Environmental Calculator
+//
+//  Created by Angelo Godbey on 5/19/14.
+//  Copyright (c) 2014 Basel Action Network. All rights reserved.
+//
+
+#import "Calculator.h"
+#import "RecycledMaterialObject.h"
+
+@implementation Calculator
+
+@end
